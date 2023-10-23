40dd915ef3a8fab2e46d31ab438bc94bad594f34
Majo <git@maxoverlack.dev>
2023-10-23 17:26:52 +0200

    Change name again

--- src/consts.ts
+++ src/consts.ts
@@ -1,7 +1,7 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'FI Blog';
+export const SITE_TITLE = 'FI-Blog';
 export const SITE_DESCRIPTION =
 	'Mein Blog für den Kurs fortgeschrittene Interaktionstechnologien der ' +
 	'Hochschule Rhein-Waal.';
